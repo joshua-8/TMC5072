68bc6f0d6ed653fc2d624d25134d651c16997ffa
joshua-8 <joshua_phelps@brown.edu>
2023-10-22 19:51:15 -0400

    add library.properties

--- src/TMC5072.h
+++ src/TMC5072.h
@@ -1,14 +1,29 @@
 #ifndef TMC5072_H
 #define TMC5072_H
 #include <Arduino.h>
+#include <SPI.h>
 /**
  * @brief
  */
 class TMC5072 {
 protected:
+    SPIClass& spi;
+    SPISettings spiset;
+
 public:
-    TMC5072()
+    TMC5072(SPIClass& _spi, SPISettings _spiset)
+        : spi(_spi)
+        , spiset(_spiset)
     {
+    }
+    void comm(uint8_t reg, uint32_t send, uint8_t& status, uint32_t& data)
+    {
+        spi.beginTransaction(spiset);
+        digitalWrite(spi.pinSS(), LOW);
+        status = spi.transfer(reg);
+        data = spi.transfer32(send);
+        digitalWrite(spi.pinSS(), HIGH);
+        spi.endTransaction();
     }
 };
 #endif // TMC5072_H
--- src/demo.ino
+++ src/demo.ino
@@ -1,23 +1,53 @@
 #include "trinamic/tmc/ic/TMC5072/TMC5072.h"
 #include <Arduino.h>
 #include <SPI.h>
+SPISettings spiset = SPISettings(100000, SPI_MSBFIRST, SPI_MODE3);
 void setup()
 {
     Serial.begin(115200);
     SPI.begin(SCK, MISO, MOSI, SS);
     pinMode(SPI.pinSS(), OUTPUT);
     digitalWrite(SPI.pinSS(), HIGH);
+    delay(100);
+
+    commAndPrint(SPI, spiset, TMC_WRITE_BIT + TMC5072_IHOLD_IRUN(1), 15 | 15 << 8);
+
+    // commAndPrint(SPI, spiset, TMC_WRITE_BIT + TMC5072_V1(1), 5000);
+
+    commAndPrint(SPI, spiset, TMC_WRITE_BIT + TMC5072_RAMPMODE(1), 1);
+    commAndPrint(SPI, spiset, TMC5072_RAMPMODE(1), 0);
+
+    commAndPrint(SPI, spiset, TMC_WRITE_BIT + TMC5072_CHOPCONF(1), TMC5072_TOFF_MASK & 4); // turns on
+
+    // commAndPrint(SPI, spiset, TMC_WRITE_BIT + TMC5072_A1(1), 100); //
+    // commAndPrint(SPI, spiset, TMC_WRITE_BIT + TMC5072_V1(1), 500); //
 }
 void loop()
 {
-    SPI.beginTransaction(SPISettings(10000, SPI_MSBFIRST, SPI_MODE3));
-    digitalWrite(SPI.pinSS(), LOW);
-    uint8_t a = SPI.transfer(TMC5072_INPUT);
-    uint32_t b = SPI.transfer32(0);
-    digitalWrite(SPI.pinSS(), HIGH);
-    SPI.endTransaction();
-    Serial.println(a, BIN);
+    Serial.println("###");
+    commAndPrint(SPI, spiset, TMC_WRITE_BIT + TMC5072_AMAX(1), 5000);
+    commAndPrint(SPI, spiset, TMC_WRITE_BIT + TMC5072_VMAX(1), 500000);
+    delay(8000);
+    commAndPrint(SPI, spiset, TMC_WRITE_BIT + TMC5072_AMAX(1), 50000);
+    commAndPrint(SPI, spiset, TMC_WRITE_BIT + TMC5072_VMAX(1), 10000);
+    delay(4000);
+}
+
+void commAndPrint(SPIClass& spi, SPISettings spiSettings, uint8_t reg, uint32_t send)
+{
+    uint8_t a;
+    uint32_t b;
+    comm(spi, spiSettings, reg, send, a, b);
+    Serial.print(a, BIN);
+    Serial.print(" ");
     Serial.println(b, BIN);
-    Serial.println();
-    delay(1000);
+}
+void comm(SPIClass& spi, SPISettings spiSettings, uint8_t reg, uint32_t send, uint8_t& status, uint32_t& data)
+{
+    spi.beginTransaction(spiSettings);
+    digitalWrite(spi.pinSS(), LOW);
+    status = spi.transfer(reg);
+    data = spi.transfer32(send);
+    digitalWrite(spi.pinSS(), HIGH);
+    spi.endTransaction();
 }
