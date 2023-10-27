45021f243633c6baade4cb29785d119226605cbb
joshua-8 <joshua_phelps@brown.edu>
2023-10-27 15:53:53 -0400

    ramp mode in progress

--- src/TMC5072.h
+++ src/TMC5072.h
@@ -1,8 +1,21 @@
 #ifndef LIB_TMC5072_H
 #define LIB_TMC5072_H
-#include "trinamic/tmc/ic/TMC5072/TMC5072.h"
 #include <Arduino.h>
 #include <SPI.h>
+
+#include "trinamic/tmc/ic/TMC5072/TMC5072.h"
+
+/**
+ * macro that helps with accessing registers with less typing
+ * @param write: true or false, should the write bit be set?
+ * @param register: the name of the register, not including TMC5072_, for example: VMAX
+ * @param whichMotor: 0 or 1, which motor
+ * @param value: the value that gets shifted, masked.
+ * becomes register,data
+ * @note only works for registers that have one for each motor, and when only one value is going into the register but that covers a lot of cases
+ * */
+#define TMC5072_COMMPACT(write, register, whichMotor, value) (write ? TMC_WRITE_BIT : 0) + TMC5072_##register(whichMotor), TMC5072_##register##_MASK&(value << TMC5072_##register##_SHIFT)
+
 /**
  * @brief
  */
@@ -10,23 +23,36 @@ class TMC5072 {
 protected:
     SPIClass& spi;
     SPISettings spiset;
-    boolean whichMotor;
+    byte whichMotor;
 
 public:
-    TMC5072(SPIClass& _spi, SPISettings _spiset, boolean _whichMotor)
+    TMC5072(SPIClass& _spi, SPISettings _spiset, byte _whichMotor)
         : spi(_spi)
         , spiset(_spiset)
     {
-        whichMotor = _whichMotor;
+        whichMotor = _whichMotor & 1;
+    }
+    void setVelAndAccelRaw(uint32_t vmax, uint16_t amax, uint16_t dmax)
+    {
+        commWrite(TMC5072_COMMPACT(true, VMAX, whichMotor, vmax));
+        if (amax != 0)
+            commWrite(TMC5072_COMMPACT(true, AMAX, whichMotor, amax));
+        if (dmax != 0)
+            commWrite(TMC5072_COMMPACT(true, DMAX, whichMotor, dmax));
+    }
+    void setTargetRaw(int32_t target)
+    {
+        commWrite(TMC5072_COMMPACT(true, RAMPMODE, whichMotor, 0));
+        commWrite(TMC5072_COMMPACT(true, XTARGET, whichMotor, target));
     }
     /**
      * @brief turns on power to the motor
-     * @param  toff: 1-15, "sets the slow decay off time" see datasheet pate 57
+     * @param  toff: 1-15, "sets the slow decay off time" see datasheet page 57
      * @retval None
      */
     void enable(byte toff = 4)
     {
-        commWrite(TMC_WRITE_BIT + TMC5072_CHOPCONF(whichMotor), TMC5072_TOFF_MASK & toff); // turns on
+        commWrite(TMC_WRITE_BIT + TMC5072_CHOPCONF(whichMotor), TMC5072_TOFF_MASK & (toff << TMC5072_TOFF_SHIFT)); // turns on
     }
     /**
      * @brief  turns off power to the motor and allows it to turn freely
@@ -45,21 +71,20 @@ public:
      */
     void setCurrent(byte run, byte hold)
     {
-        commWrite(TMC_WRITE_BIT + TMC5072_IHOLD_IRUN(whichMotor), (hold & TMC5072_IHOLD_MASK) | (TMC5072_IRUN_MASK & (run << TMC5072_IRUN_SHIFT)));
+        commWrite(TMC_WRITE_BIT + TMC5072_IHOLD_IRUN(whichMotor), (TMC5072_IHOLD_MASK & (run << TMC5072_IHOLD_SHIFT)) | (TMC5072_IRUN_MASK & (run << TMC5072_IRUN_SHIFT)));
     }
     /**
-     * @brief
-     * @note
-     * @param  speed:
-     * @param  accel:
+     * @brief sets the driver to velocity mode and sets a velocity target and optionally changes the acceleration
+     * @param  speed: (int32_t) velocity, no unit conversion
+     * @param  accel: (uint32_t) accel, no unit conversion
      * @retval None
      */
-    void setVel(int32_t speed, uint32_t accel = 0)
+    void setVelRaw(int32_t speed, uint32_t accel = 0)
     {
-        commWrite(TMC_WRITE_BIT + TMC5072_RAMPMODE(whichMotor), speed >= 0 ? 1 : 2);
+        commWrite(TMC5072_COMMPACT(true, RAMPMODE, whichMotor, speed >= 0 ? 1 : 2));
         if (accel != 0)
-            commWrite(TMC_WRITE_BIT + TMC5072_AMAX(whichMotor), accel);
-        commWrite(TMC_WRITE_BIT + TMC5072_VMAX(whichMotor), abs(speed));
+            commWrite(TMC5072_COMMPACT(true, AMAX, whichMotor, accel));
+        commWrite(TMC5072_COMMPACT(true, VMAX, whichMotor, abs(speed)));
     }
     /**
      * @brief calls spi.begin and sets up chip select pin
@@ -73,9 +98,19 @@ public:
     void beginSPI(byte SCK, byte SDI, byte SDO, byte CS)
     {
         SPI.begin(SCK, MISO, MOSI, SS);
+        beginSPIcs(CS);
+    }
+    /**
+     * @brief call this for every driver IC with which pin is connected to the driver's chip select pin
+     * @param  CS: chip select, AKA SS
+     * @retval None
+     */
+    void beginSPIcs(byte CS)
+    {
         pinMode(SPI.pinSS(), OUTPUT);
         digitalWrite(SPI.pinSS(), HIGH);
     }
+
     /**
      * @brief
      * @note
