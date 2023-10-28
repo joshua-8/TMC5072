f7b27f6420010b918d63b9b4e38c103be8e72140
joshua-8 <joshua-8@users.noreply.github.com>
2023-10-28 05:25:07 +0000

    deploy: 1890c24c0beec794fe337b3e72bd3b665ad03161

--- search/all_a.js
+++ search/all_a.js
@@ -4,6 +4,6 @@ var searchData=
   ['merchantability_1',['MERCHANTABILITY',['../license_8txt.html#a8394add39a196c4852caa8c0e5450f37',1,'license.txt']]],
   ['merge_2',['merge',['../license_8txt.html#aad4b713ca657a67ecbb454b02ceabff8',1,'license.txt']]],
   ['modify_3',['modify',['../license_8txt.html#a84db115c842c0bc29f97fd73629d8163',1,'license.txt']]],
-  ['movepositionraw_4',['movePositionRaw',['../class_t_m_c5072.html#a2c491169535e05b5f4a0a2aeab7944f6',1,'TMC5072']]],
+  ['movetopositionraw_4',['moveToPositionRaw',['../class_t_m_c5072.html#a0547810fa446ce29d32d96604e1dc377',1,'TMC5072']]],
   ['movevelraw_5',['moveVelRaw',['../class_t_m_c5072.html#ad104c9b6ba25dde0cc46806049a1e182',1,'TMC5072']]]
 ];
--- search/functions_7.js
+++ search/functions_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['movepositionraw_0',['movePositionRaw',['../class_t_m_c5072.html#a2c491169535e05b5f4a0a2aeab7944f6',1,'TMC5072']]],
+  ['movetopositionraw_0',['moveToPositionRaw',['../class_t_m_c5072.html#a0547810fa446ce29d32d96604e1dc377',1,'TMC5072']]],
   ['movevelraw_1',['moveVelRaw',['../class_t_m_c5072.html#ad104c9b6ba25dde0cc46806049a1e182',1,'TMC5072']]]
 ];
