7c10537b3854d1bbf9763b2fc2ee79d1a2fed9ea
joshua-8 <joshua_phelps@brown.edu>
2023-10-27 22:52:49 -0400

    added position control

---
+++ examples/position_demo/position_demo.ino
@@ -0,0 +1,43 @@
+/*
+ * This example for the https://github.com/joshua-8/tmc5072 library demonstrates velocity control
+ * Motor2 will start spinning then stop spinning, and accelerate quickly but slow down slowly.
+ */
+
+#include "TMC5072.h"
+
+TMC5072 driver1 = TMC5072(SPI, SPISettings(100000, SPI_MSBFIRST, SPI_MODE3), 1);
+void setup()
+{
+    Serial.begin(115200);
+    driver1.beginSPI(SCK, MISO, MOSI, SS);
+    driver1setup();
+}
+void loop()
+{
+    if (driver1.getDriverResetFlag(false, false)) {
+        Serial.println("driver restarted");
+        driver1setup();
+    }
+    driver1.movePositionRaw(70000);
+
+    delay(5000);
+
+    driver1.movePositionRaw(0);
+
+    do { // blocking wait until at target
+        delay(1);
+
+        if (driver1.getDriverResetFlag(false, false)) {
+            Serial.println("driver restarted");
+            driver1setup();
+        }
+
+    } while (driver1.isNotAtTarget());
+}
+void driver1setup()
+{
+    driver1.clearGSTAT();
+    driver1.setCurrent(16, 1);
+    driver1.setVelocitiesAndAccelerationsRaw(5000, 50000, 300, 150, 1500, 3000);
+    driver1.enable();
+}
--- src/TMC5072.h
+++ src/TMC5072.h
@@ -14,7 +14,7 @@
  * becomes register,data
  * @note only works for registers that have one for each motor, and when only one value is going into the register but that covers a lot of cases
  * */
-#define TMC5072_COMMPACT(write, register, whichMotor, value) (write ? TMC_WRITE_BIT : 0) + TMC5072_##register(whichMotor), TMC5072_##register##_MASK&(value << TMC5072_##register##_SHIFT)
+#define tmc_commpact(write, register, whichMotor, value) (write ? TMC_WRITE_BIT : 0) + TMC5072_##register(whichMotor), TMC5072_##register##_MASK&(value << TMC5072_##register##_SHIFT)
 
 /**
  * @brief
@@ -24,6 +24,8 @@ protected:
     SPIClass& spi;
     SPISettings spiset;
     byte whichMotor;
+    uint8_t statusbits;
+    uint32_t rampstatbits;
 
 public:
     TMC5072(SPIClass& _spi, SPISettings _spiset, byte _whichMotor)
@@ -31,19 +33,65 @@ public:
         , spiset(_spiset)
     {
         whichMotor = _whichMotor & 1;
+        statusbits = 0;
+        rampstatbits = 0;
     }
+    void movePositionRaw(int32_t target)
+    {
+        commWrite(tmc_commpact(true, RAMPMODE, whichMotor, 0));
+        commWrite(tmc_commpact(true, XTARGET, whichMotor, target));
+    }
+
+    void setVelocitiesAndAccelerationsRaw(uint32_t v1, uint32_t vmax, uint32_t a1, uint32_t amax, uint32_t dmax, uint32_t d1)
+    {
+        commWrite(tmc_commpact(true, V1, whichMotor, v1));
+        commWrite(tmc_commpact(true, VMAX, whichMotor, vmax));
+        commWrite(tmc_commpact(true, A1, whichMotor, a1));
+        commWrite(tmc_commpact(true, AMAX, whichMotor, amax));
+        commWrite(tmc_commpact(true, DMAX, whichMotor, dmax));
+        commWrite(tmc_commpact(true, D1, whichMotor, d1));
+    }
+
+    /**
+     * @brief
+     * @note   The datasheet says that VSTART should be >= VSTOP
+     * @param  vstart:
+     * @param  vstop:
+     * @retval None
+     */
+    void setVStopAndVStartRaw(uint32_t vstart, uint32_t vstop)
+    {
+        commWrite(tmc_commpact(true, VSTOP, 1, vstart));
+        commWrite(tmc_commpact(true, VSTART, 1, vstop));
+    }
+
+    /**
+     * @brief  Sets velocity and acceleration settings for velocity mode
+     * @param  vmax:
+     * @param  amax:
+     * @param  dmax:
+     * @retval None
+     */
     void setVelAndAccelRaw(uint32_t vmax, uint16_t amax, uint16_t dmax)
     {
-        commWrite(TMC5072_COMMPACT(true, VMAX, whichMotor, vmax));
+        commWrite(tmc_commpact(true, VMAX, whichMotor, vmax));
         if (amax != 0)
-            commWrite(TMC5072_COMMPACT(true, AMAX, whichMotor, amax));
+            commWrite(tmc_commpact(true, AMAX, whichMotor, amax));
         if (dmax != 0)
-            commWrite(TMC5072_COMMPACT(true, DMAX, whichMotor, dmax));
+            commWrite(tmc_commpact(true, DMAX, whichMotor, dmax));
     }
-    void setTargetRaw(int32_t target)
+    /**
+     * @brief sets the driver to velocity mode and sets a velocity target and optionally changes the acceleration
+     * @param  speed: (int32_t) velocity, no unit conversion
+     * @param  accel: (uint32_t) accel, no unit conversion
+     * @retval None
+     */
+    void moveVelRaw(int32_t speed, uint32_t accel = 0)
     {
-        commWrite(TMC5072_COMMPACT(true, RAMPMODE, whichMotor, 0));
-        commWrite(TMC5072_COMMPACT(true, XTARGET, whichMotor, target));
+        commWrite(tmc_commpact(true, RAMPMODE, whichMotor, speed >= 0 ? 1 : 2));
+        if (accel != 0)
+            commWrite(tmc_commpact(true, AMAX, whichMotor, accel));
+        commWrite(tmc_commpact(true, VMAX, whichMotor, abs(speed)));
     }
     /**
      * @brief turns on power to the motor
@@ -73,26 +121,14 @@ public:
     {
         commWrite(TMC_WRITE_BIT + TMC5072_IHOLD_IRUN(whichMotor), (TMC5072_IHOLD_MASK & (run << TMC5072_IHOLD_SHIFT)) | (TMC5072_IRUN_MASK & (run << TMC5072_IRUN_SHIFT)));
     }
-    /**
-     * @brief sets the driver to velocity mode and sets a velocity target and optionally changes the acceleration
-     * @param  speed: (int32_t) velocity, no unit conversion
-     * @param  accel: (uint32_t) accel, no unit conversion
-     * @retval None
-     */
-    void setVelRaw(int32_t speed, uint32_t accel = 0)
-    {
-        commWrite(TMC5072_COMMPACT(true, RAMPMODE, whichMotor, speed >= 0 ? 1 : 2));
-        if (accel != 0)
-            commWrite(TMC5072_COMMPACT(true, AMAX, whichMotor, accel));
-        commWrite(TMC5072_COMMPACT(true, VMAX, whichMotor, abs(speed)));
-    }
+
     /**
      * @brief calls spi.begin and sets up chip select pin
      * @note only needed once per SPI bus
      * @param  SCK: clock
      * @param  SDI: AKA CIPO and MISO
      * @param  SDO: AKA COPI and MOSI
-     * @param  CS: chip select, AKA SS
+     * @param  CS: chip select pin for this driver, AKA SS
      * @retval None
      */
     void beginSPI(byte SCK, byte SDI, byte SDO, byte CS)
@@ -111,6 +147,63 @@ public:
         digitalWrite(SPI.pinSS(), HIGH);
     }
 
+    boolean isNotAtTarget(boolean read = true)
+    {
+        return !isAtTarget(read);
+    }
+    boolean isAtTarget(boolean read = true)
+    {
+        return (getRampStatusBits(read) >> 9) & 1;
+    }
+
+    uint16 getRampStatusBits(boolean read = true)
+    {
+        if (read)
+            comm(TMC5072_RAMPSTAT(whichMotor), 0, &rampstatbits);
+        return rampstatbits;
+    }
+
+    boolean getStopSwitchStatus(boolean read = true)
+    {
+        return (getStatusBits(read) >> (whichMotor + 5)) & 1; // left stop switch
+    }
+
+    boolean getVelocityReachedStatus(boolean read = true)
+    {
+        return (getStatusBits(read) >> (whichMotor + 3)) & 1; // whether reached target velocity
+    }
+
+    boolean getDriverErrorStatus(boolean read = true, boolean clear = false)
+    {
+        boolean status = (getStatusBits(read) >> (whichMotor + 1)) & 1; // whether reached target velocity
+        if (clear && status) {
+            clearGSTAT();
+        }
+        return status;
+    }
+
+    boolean getDriverResetFlag(boolean read = true, boolean clear = false)
+    {
+        boolean status = (getStatusBits(read)) & 1;
+        if (clear && status) {
+            clearGSTAT();
+        }
+        return status;
+    }
+    void clearGSTAT()
+    {
+        comm(TMC5072_GSTAT, 0, nullptr);
+    }
+
+    uint8_t getStatusBits(boolean read = true)
+    {
+        if (read) { // updated read
+            // status returned for all communication, reading ifcnt is arbitrary, but safe since it's read only
+            comm(TMC5072_IFCNT, 0, nullptr); // comm sets the statusbits variable internally
+        }
+        return statusbits;
+    }
+
     /**
      * @brief
      * @note
@@ -120,12 +213,7 @@ public:
      */
     void commWrite(uint8_t reg, uint32_t send)
     {
-        spi.beginTransaction(spiset);
-        digitalWrite(spi.pinSS(), LOW);
-        spi.transfer(reg);
-        spi.transfer32(send);
-        digitalWrite(spi.pinSS(), HIGH);
-        spi.endTransaction();
+        comm(reg, send, nullptr);
     }
     /**
      * @brief
@@ -136,12 +224,17 @@ public:
      * @param  data:
      * @retval None
      */
-    void comm(uint8_t reg, uint32_t send, uint8_t& status, uint32_t& data)
+    void comm(uint8_t reg, uint32_t send, uint32_t* data)
     {
         spi.beginTransaction(spiset);
         digitalWrite(spi.pinSS(), LOW);
-        status = spi.transfer(reg);
-        data = spi.transfer32(send);
+
+        statusbits = spi.transfer(reg);
+
+        uint32_t databits = spi.transfer32(send);
+        if (data != NULL)
+            *data = databits;
+
         digitalWrite(spi.pinSS(), HIGH);
         spi.endTransaction();
     }
